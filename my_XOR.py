6eabf0fc2161bffb2334e399a81cfa27d18b0a3c
HappyHackingHigh <36451412+HappyHackingHigh@users.noreply.github.com>
2018-03-20 00:17:04 +0800

    Update my_XOR.py

--- my_XOR.py
+++ my_XOR.py
@@ -18,3 +18,11 @@ print('加密過的密文:'+encrypted)
 decrypted = crypt(encrypted, key)
 print('解密過的答案:'+decrypted)
 print('使用的金鑰:'+ key)
+
+
+“””
+itertools反覆運算器的特點是：惰性求值（Lazy evaluation）
+只有當反覆運算至某個值時，它才會被計算
+這個特點使得反覆運算器特別適合於遍歷大檔案或無限集合等
+我們不用一次性將它們存儲在記憶體中。
+“””
