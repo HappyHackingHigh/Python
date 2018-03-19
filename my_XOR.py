56be554b927c5347a27b01e51ad42a709320efcc
HappyHackingHigh <36451412+HappyHackingHigh@users.noreply.github.com>
2018-03-20 00:19:30 +0800

    Update my_XOR.py

--- my_XOR.py
+++ my_XOR.py
@@ -26,3 +26,15 @@ itertools反覆運算器的特點是：惰性求值（Lazy evaluation）
 這個特點使得反覆運算器特別適合於遍歷大檔案或無限集合等
 我們不用一次性將它們存儲在記憶體中。
 “””
+
+“””
+itertools 模組提供了三個函數(類別class)用來產生一個無限序列反覆運算器：
+count(firstval=0, step=1)
+創建一個從 firstval (預設值為 0) 開始，以 step (預設值為 1) 為step的的無限整數反覆運算器
+
+cycle(iterable)
+對 iterable 中的元素反復執行迴圈，返回反覆運算器
+
+repeat(object [,times]
+反復生成 object，如果給定 times，則重複次數為 times，否則為無限
+“””
